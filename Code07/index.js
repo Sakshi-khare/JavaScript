563f2a370868c238bdf0c42e2f5fee5463e737de
sakshi khare <sakshikhare2006@gmail.com>
2026-05-20 13:52:12 +0530

    Practice

---
+++ Code07/index.js
@@ -0,0 +1,21 @@
+// ====================================================
+
+// Strings => sequence of charcters represent text
+
+// ===================================================
+
+// CREATE STRING 
+
+let str = "SAKSHI";    // 1st Method to create string
+let str2= "hello";    // 2nd Method
+
+// String has some inbuilt properties and functions . Functions are also know as method
+
+// String Length = to print length of string 
+
+
+// ===========================================================
+
+// Template Literals in JS => way to have embedded expressions in strings
+
+//     `this is template literal`
